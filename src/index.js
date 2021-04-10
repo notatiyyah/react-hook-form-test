8255e2c35054a9c7a74c37c1a9cdc5cd54985003
AtiyyahRawat <atiyyah.rawat@gmail.com>
2021-04-10 15:17:41 +0100

    Integrate validation with form

--- src/index.js
+++ src/index.js
@@ -11,25 +11,27 @@ export default function App() {
   
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-        <input type="text" placeholder="ni" {...register("ni", {required: true, maxLength: 9, pattern: /^\s*[a-zA-Z]{2}(?:\s*\d\s*){6}[a-zA-Z]\s*$/i})} />
         
-        <div class="govuk-form-group govuk-form-group--error lbh-form-group">
-          <label class="govuk-label lbh-label" for="input-with-error-message">
+        <div class={`"govuk-form-group govuk-form-group${ errors.ni ? "--error" : "" } lbh-form-group"`}>
+          <label class="govuk-label lbh-label" for={`"input${ errors.ni ? "-with-error-message" : "" }"`}>
             National Insurance number
           </label>
-          <span id="input-with-error-message-hint" class="govuk-hint lbh-hint">
+          <span id={`"input${ errors.ni ? "-with-error-message" : "" }-hint"`} class="govuk-hint lbh-hint">
             It’s on your National Insurance card, benefit letter, payslip or P60. For
             example, ‘QQ 12 34 56 C’.
           </span>
+          {errors.ni && (
           <span id="input-with-error-message-error" class="govuk-error-message">
-            <span class="govuk-visually-hidden">Error:</span> Error message goes here
+            <span class="govuk-visually-hidden">Error:</span> Please enter a valid NI Number
           </span>
+          )}          
           <input
-            class="govuk-input lbh-input govuk-input--error"
-            id="input-with-error-message"
+            class={`"govuk-input lbh-input govuk-input${ errors.ni ? "--error" : "" }"`}
+            id={`"input${ errors.ni ? "-with-error-message" : "" }"`}
             name="test-name-3"
             type="text"
-            aria-describedby="input-with-error-message-hint input-with-error-message-error"
+            aria-describedby={`"input-${ errors.ni ? "-with-error-message" : "" }-hint input${ errors.ni ? "-with-error-message" : "" }-error"`}
+            {...register("ni", {required: true, maxLength: 9, pattern: /^\s*[a-zA-Z]{2}(?:\s*\d\s*){6}[a-zA-Z]\s*$/i})}
           />
         </div>
         
